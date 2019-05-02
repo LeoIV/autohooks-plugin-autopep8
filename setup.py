1c17711f110a30b337942998ee7db864601ae8f2
Leonard Papenmeier <leonard.papenmeier@img.ly>
2019-05-02 11:34:22 +0200

    update version after release

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from distutils.core import setup
 from pkg_resources import safe_version
 from setuptools import find_namespace_packages
 
-VERSION = (0, 0, 2)
+VERSION = (0, 0, 3, 'alpha')
 
 
 def get_version():
